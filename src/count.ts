e6d6d9e9789e5c1c62c50ae23230ba9d61cf7fbc
Denis Carriere <carriere.denis@gmail.com>
2023-12-11 00:57:21 -0500

    add log update

--- src/count.ts
+++ src/count.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import readline from "readline";
 import { Filter } from "./filter.js";
+import logUpdate from 'log-update';
 
 const tokens = new Map<string, number>();
 const writer = fs.createWriteStream("eoss-snapshot.csv");
@@ -11,10 +12,19 @@ const rl = readline.createInterface({
   crlfDelay: Infinity
 });
 
+let timestamp = 0
+let total = 0;
+
 // read each line of the file and parse it as JSON
 rl.on('line', (line) => {
   const { to } = JSON.parse(line) as Filter;
   tokens.set(to, 1 + (tokens.get(to) || 0));
+  total += 1;
+  const now = Math.floor(Date.now() / 1000);
+  if ( timestamp !== now ) {
+    timestamp = now;
+    logUpdate(`${timestamp} ${total}\n`);
+  }
 });
 
 // log the parsed JSON objects once the file has been fully read
--- src/download.ts
+++ src/download.ts
@@ -5,6 +5,7 @@ import { token, baseUrl, manifest, outputModule, params, startBlockNum, stopBloc
 import { readCursor, saveCursor } from "./utils.js";
 import fs from "fs";
 import { toTransactionId } from "./evm.js";
+import logUpdate from 'log-update';
 
 // Read Substream
 const substreamPackage = await readPackage(manifest);
@@ -34,6 +35,10 @@ export interface Download {
   rlptx: string;
 }
 
+let start = 0;
+let timestamp = 0
+let count = 0;
+
 // Stream Blocks
 const writer = fs.createWriteStream("pushtx.jsonl", {flags: "a"});
 emitter.on("anyMessage", (message: any, cursor, clock) => {
@@ -52,6 +57,17 @@ emitter.on("anyMessage", (message: any, cursor, clock) => {
         rlptx,
       }
       writer.write(JSON.stringify(row) + "\n");
+
+      // logging
+      const now = Math.floor(Date.now() / 1000);
+      if ( start === 0 ) start = now;
+      count += 1;
+      if ( timestamp !== now ) {
+        timestamp = now;
+        const elapsed = (timestamp - start);
+        const rate = Math.floor(count / elapsed);
+        logUpdate(`rate: ${rate}/s | count: ${count} | elapsed: ${elapsed} | timestamp: ${trace.blockTime.split(".")[0].replace("Z", "")} | block: ${trace.blockNum}`);
+      }
     }
   }
 });
