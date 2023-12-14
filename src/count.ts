d23974a2057ad7d5abfac27dd2fa5cf8925ee19b
Denis Carriere <carriere.denis@gmail.com>
2023-12-14 01:51:58 -0500

    add compare

--- src/count.ts
+++ src/count.ts
@@ -2,10 +2,11 @@ import fs from "fs";
 import readline from "readline";
 import { Filter } from "./filter.js";
 import logUpdate from 'log-update';
+import { getTotalFromIndex } from "./compare.js";
 
 const tokens = new Map<string, number>();
 const writer = fs.createWriteStream("eoss-snapshot.csv");
-writer.write("address,tokens\n");
+writer.write("address,tokens,index\n");
 
 // create a readline interface for reading the file line by line
 const rl = readline.createInterface({
@@ -13,26 +14,30 @@ const rl = readline.createInterface({
   crlfDelay: Infinity
 });
 
-let timestamp = 0
+let current = 0
 let total = 0;
+let last_timestamp = "";
 
 // read each line of the file and parse it as JSON
 rl.on('line', (line) => {
   if ( total >= 21000000) return;
-  const { to } = JSON.parse(line) as Filter;
+  const { to, timestamp } = JSON.parse(line) as Filter;
   tokens.set(to, 1 + (tokens.get(to) || 0));
+  last_timestamp = timestamp;
   total += 1;
   const now = Math.floor(Date.now() / 1000);
-  if ( timestamp !== now ) {
-    timestamp = now;
-    logUpdate(`total: ${total} | addresses: ${tokens.size}\n`);
+  if ( current !== now ) {
+    current = now;
+    logUpdate(`total: ${total} | addresses: ${tokens.size} | last_timestamp: ${last_timestamp}\n`);
   }
 });
 
 // log the parsed JSON objects once the file has been fully read
-rl.on('close', () => {
+rl.on('close', async () => {
   for ( const [address, count] of tokens.entries() ) {
-    writer.write(`${address},${count}\n`);
+    if ( count > 10000 ) continue;
+    const index = await getTotalFromIndex(address);
+    writer.write(`${address},${count},${index}\n`);
   }
-  logUpdate(`total: ${total} | addresses: ${tokens.size}\n`);
+  logUpdate(`total: ${total} | addresses: ${tokens.size} | last_timestamp: ${last_timestamp}\n`);
 });
